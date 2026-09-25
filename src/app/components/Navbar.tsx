f7f06d2d2b692eda57a44873ad8727422fa18dd9
fahimrahat58 <fahimrahat58@gmail.com>
2026-09-25 19:25:38 +0600

    fix my-plan btn issue

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -100,7 +100,7 @@ export default function Navbar() {
             >
               <span>Saved</span>
 
-              <span className="bg-transparent text-white border border-neutral-700 text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center">
+              <span className="min-w-6 px-2 py-1 rounded-full bg-transparent text-white border border-neutral-700 text-xs font-bold flex items-center justify-center">
                 {savedCount}
               </span>
             </Link>
